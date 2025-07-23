002c5886d33d6e90d2265978f7064f44033630f4
Sibongile <sibongilearathokwane@gmail.com>
2025-07-23 12:18:50 +0200

    added other functions

---
+++ productfunction.js
@@ -0,0 +1,14 @@
+
+prompt = require('prompt-sync')();//parseInt example
+let x= prompt ("Type any number:");//we are using a parseInt keyword
+let y=prompt ("Type another number:");;
+let nums1= parseInt(x);
+let nums2=parseInt(y);
+
+function sum(nums1,nums2){
+    this.nums1=nums1;
+    this.nums2=nums2;
+    let answer = nums1 * nums2;
+    return answer;
+}
+console.log(sum(nums1,nums2))
