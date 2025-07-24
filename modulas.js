206892dddca2a3a825ab126529e8b2ea90d985c1
Sibongile <sibongilearathokwane@gmail.com>
2025-07-24 15:50:37 +0200

    added other functions

---
+++ modulas.js
@@ -0,0 +1,10 @@
+//Anonymous function
+// it only has a variable
+let num1, num2;
+let modulus= function(num1, num2){
+    num1=100;
+    num2=5;
+    let answer = num2%num1;
+    console.log(answer)
+}
+modulus(num1, num2)
---
+++ negatefunction.js
@@ -0,0 +1,11 @@
+let num1=34
+let num2=11
+
+let subtraction = (num1,num2)=>{
+    num1=num1;
+    num2=num2;
+    let answer =num1-num2
+    console.log(answer)
+}
+subtraction(num1,num2)
+//you can use const,var and let to declare
